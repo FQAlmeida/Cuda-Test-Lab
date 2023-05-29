0b13222bd0f96199c242efe18e3a4c6fe34aeca1
Otávio Almeida <otavioalmeida650@gmail.com>
2023-05-29 17:24:56 -0300

    kinda works, but need to improv memcpys

--- src/convolution.cu
+++ src/convolution.cu
@@ -5,7 +5,7 @@
 #include <iostream>
 
 __global__ void par_convolution(uint32_t* image, uint32_t* kernel,
-                                uint32_t** out, uint32_t image_size,
+                                uint32_t* out, uint32_t image_size,
                                 uint32_t kernel_size) {
     // uint32_t tid = (blockDim.x * blockDim.y * gridDim.x * blockIdx.y) +
     //    (gridDim.x * blockDim.x * threadIdx.y) +
@@ -16,8 +16,8 @@ __global__ void par_convolution(uint32_t* image, uint32_t* kernel,
     uint32_t x = threadIdx.x + blockDim.x * blockIdx.x + padding;
     uint32_t y = threadIdx.y + blockDim.y * blockIdx.y + padding;
 
-    uint32_t line_size = blockDim.x * gridDim.x;
-    uint32_t col_size = blockDim.y * gridDim.y;
+    uint32_t line_size = blockDim.x * gridDim.x + 2 * padding;
+    // uint32_t col_size = blockDim.y * gridDim.y;
 
     uint32_t tid = x + (y * line_size);
 
@@ -26,51 +26,75 @@ __global__ void par_convolution(uint32_t* image, uint32_t* kernel,
     // } else if (y <= 0 || y >= col_size - 1) {
     //     return;
     // }
+    // printf("(%u %u) %u\n", y, x, tid);
+    uint32_t sum = 0;
+    for (uint32_t desloc_i = 0; desloc_i < kernel_size; desloc_i++) {
+        for (uint32_t desloc_j = 0; desloc_j < kernel_size; desloc_j++) {
+            uint32_t image_desloc = (x - padding + desloc_i) +
+                                    ((y - padding + desloc_j) * line_size);
 
-    printf("%d %u %u %u %u (%u %u)\n", tid, threadIdx.x, threadIdx.y,
-           blockIdx.x, blockIdx.y, x, y);
+            uint32_t pixel = image[image_desloc];
+            uint32_t kernel_pixel = kernel[desloc_i + kernel_size * desloc_j];
+            // if (pixel == 0 && kernel_pixel == 1) {
+            //     printf("(%u, %u)\n", (x - padding + desloc_i),
+            //            (y - padding + desloc_j));
+            // }
+            // printf("(%u %u) (%u %u)\t|\n", (x - padding + desloc_i),
+            //    (y - padding + desloc_j), desloc_i, desloc_j);
+            sum += pixel * kernel_pixel;
+        }
+    }
+    // printf("%d\n", sum / (kernel_size * kernel_size));
+    out[tid] = sum / (kernel_size * kernel_size);
+
+    // printf("%d %u %u %u %u (%u %u)\n", tid, threadIdx.x, threadIdx.y,
+    //        blockIdx.x, blockIdx.y, x, y);
 }
 
-void convolution(uint8_t** image, uint32_t image_size, uint8_t** kernel,
-                 uint32_t kernel_size, uint8_t*** out) {
-    dim3 grid(4, 4);
+void convolution(uint32_t** image, uint32_t image_size, uint32_t** kernel,
+                 uint32_t kernel_size, uint32_t** out) {
     uint32_t padding = (kernel_size - 1) / 2;
+
+    // TODO(Otavio): Create a better logic for grid and block dims size
+    // Make it in a way that (image_size - 2 * padding) is ways divisible
+    // Aka, all convuluted pixels should be processed, no more no less
+    dim3 grid(4, 4);
     dim3 block((image_size - 2 * padding) / 4, (image_size - 2 * padding) / 4);
 
     uint32_t *image_in_device, *image_out_device, *kernel_device;
 
     cudaMalloc((void**)&image_in_device,
-               sizeof(uint8_t) * image_size * image_size);
+               sizeof(uint32_t) * image_size * image_size);
     for (uint32_t i = 0; i < image_size; i++) {
-        cudaMemcpy(image_in_device + (i * image_size), image + i, image_size,
-                   cudaMemcpyHostToDevice);
+        cudaMemcpy(image_in_device + (i * image_size), *(image + i),
+                   image_size * sizeof(uint32_t), cudaMemcpyHostToDevice);
     }
 
-    cudaMalloc(&image_out_device, sizeof(uint8_t*) * image_size * image_size);
+    cudaMalloc(&image_out_device, sizeof(uint32_t) * image_size * image_size);
     for (uint32_t i = 0; i < image_size; i++) {
-        cudaMemcpy(image_out_device + (i * image_size), image + i, image_size,
-                   cudaMemcpyHostToDevice);
+        cudaMemcpy(image_out_device + (i * image_size), *(image + i),
+                   image_size * sizeof(uint32_t), cudaMemcpyHostToDevice);
     }
 
-    cudaMalloc(&kernel_device, sizeof(uint8_t*) * kernel_size * kernel_size);
+    cudaMalloc(&kernel_device, sizeof(uint32_t) * kernel_size * kernel_size);
     for (uint32_t i = 0; i < kernel_size; i++) {
-        cudaMemcpy(kernel_device + (i * kernel_size), kernel + i, kernel_size,
-                   cudaMemcpyHostToDevice);
+        cudaMemcpy(kernel_device + (i * kernel_size), *(kernel + i),
+                   kernel_size * sizeof(uint32_t), cudaMemcpyHostToDevice);
     }
 
     par_convolution<<<grid, block>>>(image_in_device, kernel_device,
-                                     &image_out_device, image_size,
-                                     kernel_size);
+                                     image_out_device, image_size, kernel_size);
+    cudaDeviceSynchronize();
+
     for (uint32_t i = 0; i < image_size; i++) {
-        cudaMemcpy((*out) + i, image_out_device + (i * image_size), image_size,
-                   cudaMemcpyDeviceToHost);
+        cudaMemcpy(*(out + i), image_out_device + (i * image_size),
+                   image_size * sizeof(uint32_t), cudaMemcpyDeviceToHost);
     }
 
-    cudaDeviceSynchronize();
     cudaDeviceReset();
 }
 
-void show_matrix(uint8_t** matrix, uint32_t size) {
+void show_matrix(uint32_t** matrix, uint32_t size) {
     for (uint32_t i = 0; i < size; i++) {
         for (uint32_t j = 0; j < size; j++) {
             printf("%u\t|", matrix[i][j]);
@@ -82,14 +106,14 @@ void show_matrix(uint8_t** matrix, uint32_t size) {
 int main(int argc, char const* argv[]) {
     uint32_t kernel_size = 3;
     uint32_t padding = (kernel_size - 1) / 2;
-    uint32_t n = 4;
-    uint32_t image_size = 4 * (n) + 2 * padding;
+    uint32_t n = 10;
+    uint32_t image_size = 4 * n + 2 * padding;
 
-    uint8_t** image = (uint8_t**)malloc(sizeof(uint8_t*) * image_size);
+    uint32_t** image = (uint32_t**)malloc(sizeof(uint32_t*) * image_size);
     for (uint32_t i = 0; i < image_size; i++) {
-        image[i] = (uint8_t*)malloc(sizeof(uint8_t) * image_size);
+        image[i] = (uint32_t*)malloc(sizeof(uint32_t) * image_size);
         for (uint32_t j = 0; j < image_size; j++) {
-            image[i][j] = 100;
+            image[i][j] = i;
         }
     }
     for (uint32_t i = 0; i < image_size; i++) {
@@ -99,19 +123,19 @@ int main(int argc, char const* argv[]) {
         image[image_size - 1][i] = 0;
     }
 
-    uint8_t** image_out = (uint8_t**)malloc(sizeof(uint8_t*) * image_size);
+    uint32_t** image_out = (uint32_t**)malloc(sizeof(uint32_t*) * image_size);
     for (uint32_t i = 0; i < image_size; i++) {
-        image_out[i] = (uint8_t*)malloc(sizeof(uint8_t) * image_size);
+        image_out[i] = (uint32_t*)malloc(sizeof(uint32_t) * image_size);
         for (uint32_t j = 0; j < image_size; j++) {
             image_out[i][j] = 0;
         }
     }
 
-    uint8_t** kernel = (uint8_t**)malloc(sizeof(uint8_t*) * kernel_size);
+    uint32_t** kernel = (uint32_t**)malloc(sizeof(uint32_t*) * kernel_size);
     for (uint32_t i = 0; i < kernel_size; i++) {
-        kernel[i] = (uint8_t*)malloc(sizeof(uint8_t) * kernel_size);
+        kernel[i] = (uint32_t*)malloc(sizeof(uint32_t) * kernel_size);
         for (uint32_t j = 0; j < kernel_size; j++) {
-            kernel[i][j] = 0;
+            kernel[i][j] = 1;
         }
     }
     kernel[padding][padding] = 1;
@@ -119,9 +143,10 @@ int main(int argc, char const* argv[]) {
     // show_matrix(image, image_size);
     // show_matrix(kernel, kernel_size);
 
-    convolution(image, image_size, kernel, kernel_size, &image_out);
-
-    // show_matrix(image_out, image_size);
+    convolution(image, image_size, kernel, kernel_size, image_out);
+    printf("\n");
+    show_matrix(image_out, 15);
+    // printf("%u", *(*(image_out + 10) + 100));
 
     for (uint32_t i = 0; i < image_size; i++) {
         free(image[i]);
